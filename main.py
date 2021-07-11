5334a01d43f878060740dc4dbab49beafd497dd1
Ankita-t2 <83874371+Ankita-t2@users.noreply.github.com>
2021-07-11 19:56:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-pipi[]pip
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot("Risers")
+convo = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+trainer = ListTrainer(bot)
+trainer.train(convo)
+answer = bot.get_response("Good morning")
+print(answer)
+
